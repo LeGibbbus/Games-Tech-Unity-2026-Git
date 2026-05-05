c4e0bf0ab0a623b5307388b037164b8c009dac35
LeGibbbus <biggibby29@gmail.com>
2026-05-05 11:54:19 +0100

    Testing that variables appear in unity

--- Coursework - Memento Mori/Assets/Scripts/Player_Manager.cs
+++ Coursework - Memento Mori/Assets/Scripts/Player_Manager.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Manager : MonoBehaviour
 {
+    [SerializeField] float verticalMoveSpeed = 0f;
+    [SerializeField] float horizontalMoveSpeed = 0f;
+    DefaultInputActions playerInputs;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
