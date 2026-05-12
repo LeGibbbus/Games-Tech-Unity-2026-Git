13c090a07f278c3a5d843b9be6f581c00c5000e4
LeGibbbus <biggibby29@gmail.com>
2026-05-12 14:05:22 +0100

    enemy basis

--- Coursework - Memento Mori/Assets/Scripts/Enemy_Manager.cs
+++ Coursework - Memento Mori/Assets/Scripts/Enemy_Manager.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
 public class Enemy_Manager : MonoBehaviour
+
 {
+
+    [SerializeField] float moveSpeed = 0.1f;
+    Rigidbody2D enemyBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemyBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
+        body.MovePosition(body.position + moveDirection * MoveSpeed);
     }
 }
